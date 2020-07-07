a163d93d121e045b19852bbaf09eb694c80fce67
c0derbr1t <c0der.br1t@gmail.com>
2020-07-07 16:06:06 -0500

    Create program to print a calendar (month) from command line.

--- src/14_cal.py
+++ src/14_cal.py
@@ -26,7 +26,70 @@ This would mean that from the command line you would call `python3 14_cal.py 4 2
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
 """
-
+# 🛑 STOP 
+# 🧠 UNDERSTAND
+"""
+- Research calendars with the link given.
+- What format do we want the calendar to be in
+"""
+# 🧩 PLAN
+"""
+- set up variables
+  - get current date, and transform into strings for year and month
+- make function
+  - default args for year and month, dynamically set to current year and month
+  - If no input is given:
+    - print out a text based calendar, starting with Sunday (since we are in the US)
+    - use current year and month
+  - If one input is given:
+    - print out a text based calendar, starting with Sunday
+    - Assume input is month, use current year and input
+  - If two inputs are given:
+    - print out a text based calendar, starting with Sunday
+    - Assume first input is year, and second is month, use the inputs for year and month.
+  - Add error checking
+"""
+# 💫 EXECUTE
 import sys
 import calendar
-from datetime import datetime
+from datetime import datetime, date
+
+## Current Date Variables
+current_date = str(date.today())
+split_date = current_date.split("-")
+current_year = int(split_date[0])
+current_month = int(split_date[1])
+current_day = int(split_date[2])
+
+def print_calendar(year = current_year, month = current_month):
+  text_cal = calendar.TextCalendar(6)
+  print(text_cal.formatmonth(year, month))
+
+# Arguments & Run
+if len(sys.argv) == 3:
+  if sys.argv[1].isdigit() and sys.argv[2].isdigit():
+    if int(sys.argv[2]) >= 1 and int(sys.argv[2]) <= 12:
+      print_calendar(int(sys.argv[1]), int(sys.argv[2]))
+    else:
+      print("Invalid parameter. Please enter a month between 1 and 12.")
+  else:
+    print("Invalid parameters. Please enter numbers such as YYYY and M or MM.")
+elif len(sys.argv) == 2:
+  if sys.argv[1].isdigit():
+    if int(sys.argv[1]) >= 1 and int(sys.argv[1] <= 12):
+      print_calendar(current_year, int(sys.argv[1]))
+    else:
+      print("Invalid parameter. Please enter a month between 1 and 12.")
+  else:
+    print("Invalid parameters. Please enter numbers such as M or MM.")
+else:
+  print_calendar()
+
+# 🤯 REFLECT
+"""
+- The program works well.
+- I could refactor so it takes less lines to get the current date values.
+- I could expand to print the current day as well.
+- I could add another parameter that takes into account the starting week day.
+- I could add bounds to the year to keep it realistic. (currently works with year 0 lol)
+"""
